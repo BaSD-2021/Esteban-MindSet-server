134d5427d898218614cd6dd413b8f4f32311fd80
Nicolas Cagnina <cagnina.nicolas@gmail.com>
2021-11-05 16:06:17 -0300

    create initial psychologists logic. psychologists can be listed

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const psychologists = require('./controllers/psychologists.js')
 const app = express();
 const PORT = 4000;
 
 app.get('/', (req, res) => {
     res.send('La Bestia does the best readmes ever');
 })
+
+app.get('/psychologists', psychologists.getPsychologists)
 
 app.listen(PORT, () => console.log(`Server runing on port ${PORT}`))
