0b6505e52ee374047915a747f59f3bc17eb0046a
Martin Raveglia <ravegliamartin@gmail.com>
2021-11-06 00:09:54 -0300

    MS-008: add profiles.json and folder structure with basic configuration

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const profiles = require('./controllers/profiles')
 const app = express();
 const PORT = 4000;
 
 app.get('/', (req, res) => {
     res.send('La Bestia does the best readmes ever');
 })
+
+app.get('/profiles/add', profiles.createProfile)
+
+app.get('/profiles/update/:id', profiles.updateProfile)
+
+app.get('/profiles/remove/:id', profiles.removeProfile)
+
+app.get('/profiles', profiles.listProfiles)
+
 
 app.listen(PORT, () => console.log(`Server runing on port ${PORT}`))
