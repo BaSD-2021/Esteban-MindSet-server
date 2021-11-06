083ad967284caf015064ba02b0ca9c9dec7b74f0
joaquin leimeter <leimeter.joaquin@gmail.com>
2021-11-06 02:39:01 -0300

    MS-004: create removePosition, fixed issues with updatePosition

--- controllers/positions.js
+++ controllers/positions.js
@@ -4,7 +4,7 @@ const Positions = require('../data/positions.json')
 const createPosition = (req, res) => {
   const newPosition =
     {
-      //idPosition: new Date().getTime().toString(),
+      // idPosition: new Date().getTime().toString(),
       idPosition: req.query.idPosition,
       idClient: req.query.idClient,
       jobDescription: req.query.jobDescription,
@@ -20,16 +20,18 @@ const createPosition = (req, res) => {
 }
 
 const updatePosition = (req, res) => {
-  const positionToUpdate = req.query.idPosition
-  for (const position of Positions) {
-    if(position.idPosition === positionToUpdate){
-      position.idClient = req.query.idClient,
-      position.jobDescription = req.query.jobDescription,
-      position.vacancy = req.query.vacancy,
-      position.professionalProfiles = req.query.professionalProfiles,
-      position.status = req.query.status
+  for (const position in Positions) {
+    if(Positions[position].idPosition == req.query.idPosition){
+      Positions[position].idPosition = req.query.idPosition,
+      Positions[position].idClient = req.query.idClient,
+      Positions[position].jobDescription = req.query.jobDescription,
+      Positions[position].vacancy = req.query.vacancy,
+      Positions[position].professionalProfiles = req.query.professionalProfiles,
+      Positions[position].status = req.query.status
+      console.log('entered if')
+      res.send('updated position')
+      break
     }
-    res.send(position)
   }
   fs.writeFile('./data/positions.json', JSON.stringify(Positions), {}, err => {
     if (err) throw err
@@ -37,17 +39,25 @@ const updatePosition = (req, res) => {
 }
 
 const removePosition = (req, res) => {
-
+  const positionToRemove = req.query.idPosition
+  for (const position in Positions) {
+    if(Positions[position].idPosition == positionToRemove){
+      Positions.splice(position, 1)
+      break
+    }
+  }
+  fs.writeFile('./data/positions.json', JSON.stringify(Positions), {}, err => {
+    if (err) throw err
+  })
 }
 
 const listPositions = (req, res) => {
   fs.readFile('./data/positions.json', 'utf8', (err, data) => {
     if (err) throw err;
-    console.log(data);
     res.send(JSON.parse(data))
   });
+  console.log(Positions)
 }
-
 
 module.exports = {
   createPosition,
@@ -55,7 +65,3 @@ module.exports = {
   removePosition,
   listPositions
 }
-
-
-
-//localhost:4000/positions/create?idSession=1&idClient=1&jobDescription=Treasury Expert&vacancy=3&professionalProfiles[]=Treasury Expert&professionalProfiles[]=Treasury&status=open
