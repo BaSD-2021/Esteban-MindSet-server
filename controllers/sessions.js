c43870b6f33ea4be230f9fce045f47f01528a20e
ripellizzari <ripellizzari@gmail.com>
2021-11-06 11:45:13 -0300

    Fix createSession error on server

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,9 +1,7 @@
 const fs = require('fs')
 const Sessions = require('../data/sessions.json')
 
-
-
-const createSessions = (req, res) => {
+const createSession = (req, res) => {
 
     const newSession = {
         idSession: req.query.idSession,
@@ -16,21 +14,20 @@ const createSessions = (req, res) => {
 
 
 
-    fs.writeFile('./data/clients.json', JSON.stringify(Clients), {}, (error) => {
+    fs.writeFile('./data/sessions.json', JSON.stringify(Sessions), {}, (error) => {
         if (error) {
             res.status(400).send(error)
         } else {
-            res.status(201).json(newClient)
+            res.status(201).json(newSession)
         }
 
     }
-
     )
 }
-
 // Here we have to put the functions that we want to export
 module.exports = {
-    createSessions,
+    createSession,
+
 }
 
 
--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 const express = require('express');
 const app = express();
 const PORT = 4000;
-const sessions = ('./controllers/sessions.js')
-
-
-
+const sessions = require('./controllers/sessions')
 
 app.get('/', (req, res) => {
     res.send('La Bestia does the best readmes ever');
 })
 
-//Here we have the sessions
-app.get('/sessions', sessions.listSessions)
 app.get('/sessions/create', sessions.createSession)
-app.get('/sessions/remove', sessions.removeSession)
-app.get('/sessions/update', sessions.updateSession)
 
 
+
+//Here we have the sessions
+// app.get('/sessions', sessions.listSessions)
+// app.get('/sessions/remove', sessions.removeSession)
+// app.get('/sessions/update', sessions.updateSession)
 
 app.listen(PORT, () => console.log(`Server runing on port ${PORT}`))
 
