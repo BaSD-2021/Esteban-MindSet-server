4198c1cd469bfc81ed53b14977149abe14e6a54d
joaquin leimeter <leimeter.joaquin@gmail.com>
2021-11-05 22:33:28 -0300

    MS-004: create position logic and .json

---
+++ controllers/positions.js
@@ -0,0 +1,45 @@
+const fs = require('fs')
+const Positions = require('../data/positions.json')
+
+const createPosition = (req, res) => {
+  const newPosition =
+    {
+      idPosition: new Date().getTime().toString(),
+      idClient: req.query.idClient,
+      jobDescription: req.query.jobDescription,
+      vacancy: req.query.vacancy,
+      professionalProfiles: req.query.professionalProfiles,
+      status: req.query.status
+    }
+    Positions.push(newPosition)
+    fs.writeFile('./data/positions.json', JSON.stringify(Positions), {}, err => {
+      if (err) throw err
+    })
+    res.send('create position')
+}
+
+const updatePosition = () => {
+}
+
+const removePosition = (req, res) => {
+
+}
+
+const listPositions = (req, res) => {
+  fs.readFile('./data/positions.json', (err, data) => {
+    if (err) throw err;
+    // console.log(data);
+  });
+  res.send(data)
+}
+
+module.exports = {
+  createPosition,
+  updatePosition,
+  removePosition,
+  listPositions
+}
+
+
+
+//localhost:4000/positions/create?idSession=1&idClient=1&jobDescription=Treasury Expert&vacancy=3&professionalProfiles[]=Treasury Expert&professionalProfiles[]=Treasury&status=open
--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
+const positions = require('./controllers/positions.js')
+
 app.get('/', (req, res) => {
     res.send('La Bestia does the best readmes ever');
 })
+
+//POSITIONS
+app.get('/positions', positions.listPositions)
+app.get('/positions/create', positions.createPosition)
+app.get('/positions/remove', positions.removePosition)
+app.get('/positions/update', positions.updatePosition)
 
 app.listen(PORT, () => console.log(`Server runing on port ${PORT}`))
