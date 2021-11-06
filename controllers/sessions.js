3336830a7ab65fde7be7ff72c9ab36e92c46fc7b
ripellizzari <ripellizzari@gmail.com>
2021-11-06 18:05:10 -0300

    Create update & delete sessions

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,18 +1,26 @@
 const fs = require('fs')
 const Sessions = require('../data/sessions.json')
 
+
+
+const listSession = (req, res) => {
+
+    res.json(Sessions);
+
+}
+
+
 const createSession = (req, res) => {
 
     const newSession = {
         idSession: req.query.idSession,
         idPsicologist: req.query.idPsicologist,
         idPostulant: req.query.idPostulant,
-
+        date: req.query.date,
+        state: req.query.state,
     }
 
     Sessions.push(newSession)
-
-
 
     fs.writeFile('./data/sessions.json', JSON.stringify(Sessions), {}, (error) => {
         if (error) {
@@ -20,13 +28,58 @@ const createSession = (req, res) => {
         } else {
             res.status(201).json(newSession)
         }
-
-    }
-    )
+    })
 }
-// Here we have to put the functions that we want to export
+
+
+const updateSession = (req, res) => {
+    let updatedSession
+    const updatedSessions = Sessions.map((session) => {
+        if (session.idSession == req.query.idSession) {
+            updatedSession = {
+                idSession: req.query.idSession,
+                idPsicologist: req.query.idPsicologist,
+                idPostulant: req.query.idPostulant,
+                date: req.query.date || session.date,
+                state: req.query.state || session.state
+            }
+
+            return updatedSession
+        }
+        return session
+    })
+
+    fs.writeFile('./data/sessions.json', JSON.stringify(updatedSessions), {}, (error) => {
+        if (error) {
+            res.status(400).send(error)
+        } else {
+            res.status(201).json(updatedSession)
+
+        }
+    })
+
+}
+
+
+const deleteSession = (req, res) => {
+    const filterSessions = Sessions.filter(session => session.idSession != req.query.idSession)
+
+    fs.writeFile('./data/sessions.json', JSON.stringify(Sessions), {}, (error) => {
+        if (error) {
+            res.status(400).send(error)
+        } else {
+            res.status(201).json(filterSessions)
+        }
+    })
+
+}
+
+
 module.exports = {
     createSession,
+    listSession,
+    updateSession,
+    deleteSession,
 
 }
 
--- index.js
+++ index.js
@@ -7,14 +7,11 @@ app.get('/', (req, res) => {
     res.send('La Bestia does the best readmes ever');
 })
 
-app.get('/sessions/create', sessions.createSession)
-
-
-
 //Here we have the sessions
-// app.get('/sessions', sessions.listSessions)
-// app.get('/sessions/remove', sessions.removeSession)
-// app.get('/sessions/update', sessions.updateSession)
+app.get('/sessions', sessions.listSession)
+app.get('/sessions/create', sessions.createSession)
+app.get('/sessions/delete', sessions.deleteSession)
+app.get('/sessions/update', sessions.updateSession)
 
 app.listen(PORT, () => console.log(`Server runing on port ${PORT}`))
 
