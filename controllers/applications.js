a4bcdebb3c568eab893ee5156db2c26f1da1ded4
joaquin leimeter <leimeter.joaquin@gmail.com>
2021-11-06 15:39:11 -0300

    MS-003: create backbone to get started

---
+++ controllers/applications.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const Applications = require('../data/applications.json')
+
+
+const createApplication = (req, res) => {
+
+}
+
+const updateApplication = (req, res) => {
+
+}
+
+const removeApplication = (req, res) => {
+
+}
+
+const listApplication = (req, res) => {
+
+}
+
+module.exports = {
+  createApplication,
+  updateApplication,
+  removeApplication,
+  listApplication,
+}
--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = 4000;
+const applications = require('./controllers/applications')
 
 app.get('/', (req, res) => {
     res.send('La Bestia does the best readmes ever');
 })
 
 app.listen(PORT, () => console.log(`Server runing on port ${PORT}`))
+
+//PPLICATIONS   
+app.get('/applications', applications.listApplication) 
+app.get('/applications/create', applications.createApplication) 
+app.get('/applications/update', applications.updateApplication) 
+app.get('/applications/remove', applications.removeApplication) 
