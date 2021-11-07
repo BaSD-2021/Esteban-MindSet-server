7b4c6f4d13b3ab11ac1d037b776d28e769c9fe4c
Martin Raveglia <ravegliamartin@gmail.com>
2021-11-06 21:01:47 -0300

    MS-008: add profiles.json and folder structure with basic configuration

---
+++ controllers/profiles.js
@@ -0,0 +1,13 @@
+const profiles = require('../data/profiles.json')
+
+const createProfile = (req, res) => {}
+const updateProfile = (req, res) => {}
+const removeProfile = (req, res) => {}
+const listProfiles = (req, res) => {}
+
+module.exports = {
+  createProfile,
+  updateProfile,
+  removeProfile,
+  listProfiles
+}
