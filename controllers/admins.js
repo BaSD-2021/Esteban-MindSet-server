9ec8df86c5e9ad28324871c4af559d87590ab7ef
Ignacio Aristo <ignacioaristo@gmail.com>
2021-11-07 22:34:40 -0300

    BaSD/MS-002: fix code as required in the review

--- controllers/admins.js
+++ controllers/admins.js
@@ -1,41 +1,34 @@
 const fs = require('fs');
 const admins = require('../data/admins.json');
 
-//Get all admins
-
 const getAdmins = (req, res) => {
-    res.status(200).json(
-        res.json(admins)
-    )
+	res.status(200).json(admins)
 }
-
-//Update an admin
 
 const updateAdmin = (req, res) => {
-    const found = admins.some(admin => admin.id === parseInt(req.params.id));
-
-    if (found){
-        admins.forEach(admin => {
-            if (admin.id === parseInt(req.params.id)){
-                !req.query.email ? null : admin.email = req.query.email;
-                !req.query.password ? null : admin.password = req.query.password;
-                !req.query.name ? null : admin.name = req.query.name;
-            }   
-        })
-    }   else{
-        res.status(400).json({msg: `Admin with id: ${req.params.id} was not found`})
-    }
-
-    fs.writeFile('./data/admin.json', JSON.stringify(admins), {}, (error) => {
-        if (error){
-            res.status(400).send(error)
-        }   else{
-            res.status(200).json({msg: 'Admin updated', admins})
-        }
-    })
+	const admin = admins.some(admin => admin.id === parseInt(req.params.id));
+	if(admin){
+		admins.map(admin => {
+			if (admin.id === parseInt(req.params.id)) {
+				!req.query.email ? null : admin.email = req.query.email;
+				!req.query.password ? null : admin.password = req.query.password;
+				!req.query.name ? null : admin.name = req.query.name;
+			}
+		})
+	}else {
+		res.status(400).json({ msg: `Admin with id: ${req.params.id} was not found` })
+	}
+		
+	fs.writeFile('./data/admins.json', JSON.stringify(admins), {}, (error) => {
+		if (error) {
+			return res.status(400).send(error)
+		}else {
+			return res.status(200).json({ msg: 'Admin updated', admins })
+		}
+	})
 }
-    
+
 module.exports = {
-    getAdmins,
-    updateAdmin,
+	getAdmins,
+	updateAdmin,
 };
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+const admins = require('./controllers/admins');
 const express = require('express');
 const app = express();
 const PORT = 4000;
-const admins = require('./controllers/admins');
 
 app.get('/', (req, res) => {
-    res.send('La Bestia does the best readmes ever');
+	res.send('La Bestia does the best readmes ever');
 })
 
 app.get('/admins', admins.getAdmins)
