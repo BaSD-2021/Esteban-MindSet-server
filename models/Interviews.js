41b7a30579266d5ee64ce8753e4b847adb5f7fc5
Nicolas Cagnina <cagnina.nicolas@gmail.com>
2021-11-14 02:51:11 -0300

    MS-26: fix syntax error on schema

--- models/Interviews.js
+++ models/Interviews.js
@@ -2,34 +2,35 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const InterviewSchema = new Schema({
-  postulant: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'Postulants',
+const InterviewSchema = new Schema(
+  {
+    postulant: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'Postulants',
+    },
+    client: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'Clients',
+    },
+    application: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'Applications',
+    },
+    status: {
+      type: String,
+      enum: ['succesful', 'failed', 'cancelled', 'assigned', 'confirmed'],
+      default: 'assigned',
+      required: true,
+    },
+    date: { type: Date, required: true },
+    notes: { type: String },
   },
-  client: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'Clients',
+  {
+    timestamps: true,
   },
-  application: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'Applications',
-  },
-  status: {
-    type: String,
-    enum: ['succesful', 'failed', 'cancelled', 'assigned', 'confirmed'],
-    default: 'assigned',
-    required: true,
-  },
-  date: { type: Date, required: true },
-  notes: { type: String },
-},
-{
-  timestamps: true,
-},
 );
 
 module.exports = mongoose.model('Interviews', InterviewSchema);
