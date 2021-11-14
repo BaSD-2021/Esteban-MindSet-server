1d2d88a5ade115e3c68f36a30e807d83f19093a5
Martin Raveglia <ravegliamartin@gmail.com>
2021-11-14 01:21:31 -0300

    MS-25: Add Sessions Mongoose Schema

---
+++ models/Sessions.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const SessionSchema = new Schema({
+  postulant: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Postulants',
+  },
+  psychologist: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Psychologists',
+  },
+  status: {
+    type: String,
+    enum: ['cancelled', 'assigned', 'succesful'],
+    default: 'assigned',
+    required: true,
+  },
+  date: { type: Date, required: true },
+  notes: { type: String },
+});
+
+module.exports = { SessionSchema };
