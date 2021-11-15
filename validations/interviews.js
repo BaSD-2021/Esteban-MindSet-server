642c614fa61704dcee6b2a6eacbc6a0275074543
Nicolas Cagnina <cagnina.nicolas@gmail.com>
2021-11-15 18:14:14 -0300

    MS-34: fix status-code from validateInterview function

--- validations/interviews.js
+++ validations/interviews.js
@@ -19,7 +19,7 @@ const validateInterview = (req, res, next) => {
     return res.status(400).json({ message: 'Status is wrong or missing' });
   }
   if (!bodyReq.date || !bodyReq.date.match(/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/)) {
-    return res.status(404).json({ message: 'Date is wrong or missing' });
+    return res.status(400).json({ message: 'Date is wrong or missing' });
   }
   return next();
 };
@@ -31,19 +31,19 @@ const validateUpdatedInterview = (req, res, next) => {
   };
 
   if (bodyReq.postulant && !ObjectId.isValid(bodyReq.postulant)) {
-    return res.status(404).json({ message: 'Postulant id value is incorrect' });
+    return res.status(400).json({ message: 'Postulant id value is incorrect' });
   }
   if (bodyReq.client && !ObjectId.isValid(bodyReq.client)) {
-    return res.status(404).json({ message: 'Client id value is incorrect' });
+    return res.status(400).json({ message: 'Client id value is incorrect' });
   }
   if (bodyReq.application && !ObjectId.isValid(bodyReq.application)) {
-    return res.status(404).json({ message: 'Application id value is incorrect' });
+    return res.status(400).json({ message: 'Application id value is incorrect' });
   }
   if (bodyReq.status && !bodyReq.status.match(enu)) {
     return res.status(400).json({ message: 'Status value is incorrect' });
   }
   if (bodyReq.date && !bodyReq.date.match(/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/)) {
-    return res.status(404).json({ message: 'Date value is incorrect' });
+    return res.status(400).json({ message: 'Date value is incorrect' });
   }
   return next();
 };
@@ -52,7 +52,7 @@ const validateInterviewId = (req, res, next) => {
   const paramsId = req.params.id;
 
   if (!paramsId || !ObjectId.isValid(paramsId)) {
-    return res.status(404).json({ message: 'Interview id value is incorrect' });
+    return res.status(400).json({ message: 'Interview id value is incorrect' });
   }
 
   return next();
