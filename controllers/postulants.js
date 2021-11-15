5611362450f4f7a6d67c410fc0a11d7d2380f009
Nicolas Cagnina <cagnina.nicolas@gmail.com>
2021-11-14 21:06:51 -0300

    MS-30: fix lint problems

--- controllers/postulants.js
+++ controllers/postulants.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const Postulants = require('../models/Postulants');
 
 const listPostulants = (req, res) => {
@@ -49,91 +48,41 @@ const deletePostulant = (req, res) => {
 };
 
 const updatePostulants = (req, res) => {
-  const params = req.query;
-  let updatedPostulant;
+  const bodyReq = req.body;
+  Postulants.findByIdAndUpdate(
+    req.params.id,
+    {
+      first_name: bodyReq.first_name,
+      last_name: bodyReq.last_name,
+      email: bodyReq.email,
+      password: bodyReq.password,
+      contactRange: bodyReq.contactRange,
+      address: bodyReq.address,
+      birthday: bodyReq.birthday,
+      available: bodyReq.available,
+      phone: bodyReq.phone,
+      profiles: bodyReq.profiles,
+      studies: {
+        primaryStudies: bodyReq.studies.primaryStudies,
+        secondaryStudies: bodyReq.studies.secondaryStudies,
+        tertiaryStudies: bodyReq.studies.tertiaryStudies,
+        universityStudies: bodyReq.studies.universityStudies,
+        informalStudies: bodyReq.studies.informalStudies,
+      },
+      workExperience: bodyReq.workExperience,
+    },
+    { new: true },
+    (error, newPostulant) => {
+      if (!newPostulant) {
+        return res.status(404).json('Interview id does not exist');
+      }
 
-  const updatedPostulants = Postulants.map((postulant) => {
-    if (postulant.id === params.id) {
-      updatedPostulant = {
-        id: params.id,
-        first_name: params.first_name,
-        last_name: params.last_name,
-        email: params.email,
-        phone: params.phone,
-        address: params.address,
-        birthday: params.birthday,
-        password: params.password,
-        primary_studies: {
-          date: {
-            start_date: params.start_date,
-            end_date: params.end_date,
-          },
-          school: params.school,
-        },
-        secondary_studies: {
-          date: {
-            start_date: params.start_date,
-            end_date: params.end_date,
-          },
-          school: params.school,
-        },
-        tertiary_studies: {
-          date: {
-            start_date: params.start_date,
-            end_date: params.end_date,
-          },
-          institute: params.institute,
-        },
-        university_studies: {
-          date: {
-            start_date: params.start_date,
-            end_date: params.end_date,
-          },
-          university: params.university,
-        },
-        work_experience: [{
-          company: params.company,
-          date: {
-            start_date: params.start_date,
-            end_date: params.end_date,
-          },
-          description: params.description,
-        }],
-        course: [{
-          institute: params.institute,
-          date: {
-            start_date: params.start_date,
-            end_date: params.end_date,
-          },
-          description: params.description,
-        }],
-        about: {
-          date: {
-            start_date: params.start_date,
-            end_date: params.end_date,
-          },
-          description: params.description,
-        },
-        available: params.available,
-      };
-      return updatedPostulant;
-    }
-    return postulant;
-  });
-
-  if (!updatedPostulant) {
-    return res.status(404).send('postulant not found');
-  }
-  if (JSON.stringify(updatedPostulants) === JSON.stringify(Psychologists)) {
-    return res.status(200).send('postulant found, but no changes made');
-  }
-
-  fs.writeFile('./data/postulants.json', JSON.stringify(updatedPostulants), {}, (err) => {
-    if (error) {
-      res.status(400).send(error);
-    }
-  });
-  res.send(updatedPostulants);
+      if (error) {
+        return res.status(400).json(error);
+      }
+      return res.status(201).json(newPostulant);
+    },
+  );
 };
 
 module.exports = {
