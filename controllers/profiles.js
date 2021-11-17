5b9a50926b61be9649e53fbe7242ca1ba56aec9b
joaquin leimeter <leimeter.joaquin@gmail.com>
2021-11-17 20:41:22 -0300

    MS-37: fix error message format

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -2,7 +2,7 @@ const Profiles = require('../models/Profiles');
 
 const listProfiles = (req, res) => {
   Profiles.find(req.query)
-    .then((profiles) => res.status(200).json(profiles))
+    .then((profiles) => res.status(200).json({ message: 'profiles list', data: profiles }))
     .catch((error) => res.status(400).json({ message: error }));
 };
 
@@ -15,7 +15,7 @@ const createProfile = (req, res) => {
     if (error) {
       return res.status(400).json({ message: error });
     }
-    return res.status(201).json(profile);
+    return res.status(201).json({ message: 'profile created', data: profile });
   });
 };
 
@@ -33,7 +33,7 @@ const updateProfile = (req, res) => {
       if (!newProfile) {
         return res.status(404).send({ message: 'Profile id does not exist' });
       }
-      return res.status(200).json(newProfile);
+      return res.status(200).json({ message: 'profile updated', data: newProfile });
     },
   );
 };
