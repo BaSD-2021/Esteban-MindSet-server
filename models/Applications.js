f71f19918fa4801e930725214e9558cd112f8f02
ripellizzari <ripellizzari@gmail.com>
2021-11-17 20:11:54 -0300

    MS-023: update schema structure

--- models/Applications.js
+++ models/Applications.js
@@ -4,10 +4,20 @@ const { Schema } = mongoose;
 
 const ApplicationsSchema = new Schema(
   {
-    id: { type: mongoose.Schema.Types.ObjectId },
-    positions: { type: String, required: true, ref: 'Positions' },
-    postulants: { type: String, required: true, ref: 'Postulants' },
-    interview: { type: String, ref: 'Interviews' },
+    positions: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'Positions',
+    },
+    postulants: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'Postulants',
+    },
+    interview: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Interviews',
+    },
     result: { type: String, required: true },
   },
   { timestamps: true },
