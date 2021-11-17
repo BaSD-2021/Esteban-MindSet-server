41c418f70c7fb1146a0f49c04849d16e11e0d42f
Esteban Frare <esteban.frare@radiumrocket.com>
2021-11-17 00:56:08 -0300

    MS-45: create Client views

---
+++ public/js/clientForm.js
@@ -0,0 +1,110 @@
+window.onload = () => {
+  const nameInput = document.getElementById('name');
+  const phoneInput = document.getElementById('phone');
+  const logoInput = document.getElementById('logo');
+  const addressInput = document.getElementById('address');
+  const cityInput = document.getElementById('city');
+  const countryInput = document.getElementById('country');
+  const stateInput = document.getElementById('state');
+  const descriptionInput = document.getElementById('description');
+
+  const form = document.getElementById('form');
+  const saveButton = document.getElementById('saveButton');
+  const errorMessage = document.getElementById('error_massage');
+
+  const params = new URLSearchParams(window.location.search);
+  saveButton.disabled = !!params.get('clientId');
+
+  const onFocusInput = () => {
+    errorMessage.innerText = '';
+  };
+
+  nameInput.onfocus = onFocusInput;
+  phoneInput.onfocus = onFocusInput;
+  logoInput.onfocus = onFocusInput;
+  addressInput.onfocus = onFocusInput;
+  cityInput.onfocus = onFocusInput;
+  countryInput.onfocus = onFocusInput;
+  stateInput.onfocus = onFocusInput;
+  descriptionInput.onfocus = onFocusInput;
+
+  if (params.get('clientId')) {
+    fetch(`${window.location.origin}/api/clients?_id=${params.get('clientId')}`)
+      .then((response) => {
+        if (response.status !== 200 && response.status !== 201) {
+          return response.json().then(({ message }) => {
+            throw new Error(message);
+          });
+        }
+        return response.json();
+      })
+      .then((response) => {
+        saveButton.disabled = false;
+        response.forEach((client) => {
+          nameInput.value = client.name;
+          phoneInput.value = client.phone;
+          logoInput.value = client.logo;
+          addressInput.value = client.location.address;
+          cityInput.value = client.location.city;
+          countryInput.value = client.location.country;
+          stateInput.value = client.location.state;
+          descriptionInput.value = client.description;
+        });
+      })
+      .catch((error) => {
+        errorMessage.innerText = error;
+      });
+  }
+
+  form.onsubmit = (event) => {
+    event.preventDefault();
+    saveButton.disabled = true;
+
+    let url;
+    const options = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: nameInput.value,
+        phone: parseInt(phoneInput.value, 10),
+        logo: logoInput.value,
+        description: descriptionInput.value,
+        location: {
+          address: addressInput.value,
+          city: cityInput.value,
+          country: countryInput.value,
+          state: stateInput.value,
+        },
+      }),
+    };
+
+    if (params.get('clientId')) {
+      options.method = 'PUT';
+      url = `${window.location.origin}/api/clients/${params.get('clientId')}`;
+    } else {
+      options.method = 'POST';
+      url = `${window.location.origin}/api/clients`;
+    }
+
+    fetch(url, options)
+      .then((response) => {
+        if (response.status !== 200 && response.status !== 201) {
+          return response.json().then(({ message }) => {
+            throw new Error(message);
+          });
+        }
+        return response.json();
+      })
+      .then(() => {
+        // eslint-disable-next-line no-underscore-dangle
+        window.location.href = `${window.location.origin}/views/clientList.html`;
+      })
+      .catch((error) => {
+        errorMessage.innerText = error;
+      })
+      .finally(() => {
+        saveButton.disabled = false;
+      });
+  };
+};
---
+++ public/js/clientList.js
@@ -0,0 +1,30 @@
+const openEditClientForm = (client) => {
+  // eslint-disable-next-line no-underscore-dangle
+  window.location.href = `${window.location.origin}/views/clientForm.html?clientId=${client._id}`;
+};
+
+const openNewClientForm = () => {
+  // eslint-disable-next-line no-underscore-dangle
+  window.location.href = `${window.location.origin}/views/clientForm.html`;
+};
+
+window.onload = () => {
+  const addClientButton = document.getElementById('addClient');
+  addClientButton.onclick = openNewClientForm;
+
+  const tableContent = document.getElementById('table-content');
+  fetch(`${window.location.origin}/api/clients`)
+    .then((response) => response.json())
+    .then((response) => {
+      response.forEach((client) => {
+        const tr = document.createElement('tr');
+        const NameTD = document.createElement('td');
+        const phoneTD = document.createElement('td');
+        NameTD.innerText = client.name;
+        phoneTD.innerText = client.phone;
+        tr.onclick = () => openEditClientForm(client);
+        tr.append(NameTD, phoneTD);
+        tableContent.append(tr);
+      });
+    });
+};
