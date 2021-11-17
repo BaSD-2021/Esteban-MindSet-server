c80af833bbd5c6e47f59605046f517f6e4dc837b
ripellizzari <ripellizzari@gmail.com>
2021-11-17 18:24:27 -0300

    MS-023: fix error msg

--- controllers/applications.js
+++ controllers/applications.js
@@ -16,7 +16,7 @@ const createApplication = (req, res) => {
 
   application.save((error, app) => {
     if (error) {
-      return res.status(400).json(error);
+      return res.status(400).json({ message: error });
     }
     return res.status(201).json(app);
   });
