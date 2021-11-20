c64fedb5a4100e13a5dad1165d99516803e702c1
Ignacio Aristo <ignacioaristo@gmail.com>
2021-11-19 23:42:38 -0300

    MS-53: create delete function with modal

--- public/js/profileList.js
+++ public/js/profileList.js
@@ -8,17 +8,55 @@ const openNewProfileForm = () => {
   window.location.href = `${window.location.origin}/views/profileForm.html`;
 };
 
-const deleteProfile = (id, event) => {
-  event.stopPropagation();
-};
-
 window.onload = () => {
   const navButton = document.getElementById('profilesNav');
   const tableContent = document.getElementById('table-content');
-
   const addProfileButton = document.getElementById('addProfile');
-  addProfileButton.onclick = openNewProfileForm;
 
+  // MODAL SETTINGS
+  const modalSection = document.getElementById('modal-section');
+  const openModal = () => {
+    modalSection.classList.add('modal-section-display-on');
+  };
+
+  const closeModal = (e) => {
+    if (e.target === modalSection) {
+      modalSection.classList.remove('modal-section-display-on');
+      modalSection.classList.add('modal-display-off');
+    }
+  };
+
+  window.addEventListener('click', closeModal);
+  const modalCloseBtn = document.getElementById('modal-close-button');
+  modalCloseBtn.onclick = () => {
+    modalSection.classList.remove('modal-section-display-on');
+    modalSection.classList.add('modal-display-off');
+  };
+  // END MODAL SETTING
+
+  const deleteProfile = (id, name, event) => {
+    event.stopPropagation();
+    document.getElementById('modal-title').innerText = 'You are about to delete a Professional Profile:';
+    document.getElementById('modal-data-inputs').innerText = `Profile: ${name}`;
+    openModal();
+    document.getElementById('cancel-button').onclick = () => {
+      modalSection.classList.remove('modal-section-display-on');
+      modalSection.classList.add('modal-display-off');
+    };
+    document.getElementById('procced-button').onclick = () => {
+      const url = `${window.location.origin}/api/profiles/${id}`;
+      fetch(url, {
+        method: 'DELETE',
+        headers: {
+          'Content-type': 'application/json',
+        },
+      })
+        .then(window.location.reload())
+        .catch((error) => error);
+    };
+  };
+
+  addProfileButton.onclick = openNewProfileForm;
   navButton.classList.add('activePage');
   fetch(`${window.location.origin}/api/profiles`)
     .then((response) => response.json())
@@ -32,7 +70,7 @@ window.onload = () => {
         DeleteTD.append(button);
         button.innerHTML = "<img src='https://img.icons8.com/material-rounded/24/000000/delete.png'/>";
         // eslint-disable-next-line no-underscore-dangle
-        button.onclick = (event) => deleteProfile(profile._id, event);
+        button.onclick = (event) => deleteProfile(profile._id, profile.name, event);
         tr.onclick = () => openEditProfileForm(profile);
         tr.append(NameTD, DeleteTD);
         tableContent.append(tr);
