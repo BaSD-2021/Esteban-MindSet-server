8c85a50140699eecaf6ffb5b6cdd1f7f33590af2
ripellizzari <ripellizzari@gmail.com>
2021-11-20 19:44:16 -0300

    MS-50: update position & fix validation error

--- public/js/positionForm.js
+++ public/js/positionForm.js
@@ -2,17 +2,19 @@ window.onload = () => {
   const navButton = document.getElementById('positionsNav');
   navButton.classList.add('activePage');
 
+  const params = new URLSearchParams(window.location.search);
+  const positionId = params.get('_id');
+
   const clientInput = document.getElementById('client');
   const jobDescriptionInput = document.getElementById('jobDescription');
   const vacancyInput = document.getElementById('vacancy');
   const professionalProfilesInput = document.getElementById('professionalProfiles');
   const isOpenInput = document.getElementById('isOpen');
 
-  // const form = document.getElementById('form');
+  const form = document.getElementById('form');
   const saveButton = document.getElementById('saveButton');
   const errorMessage = document.getElementById('error_msg');
 
-  const params = new URLSearchParams(window.location.search);
   saveButton.disable = !!params.get('_id');
 
   const onFocusInput = () => {
@@ -25,8 +27,8 @@ window.onload = () => {
   professionalProfilesInput.onfocus = onFocusInput;
   isOpenInput.onfocus = onFocusInput;
 
-  if (params.get('_id')) {
-    fetch(`${window.location.origin}/api/positions?_id=${params.get('_id')}`)
+  if (positionId) {
+    fetch(`${window.location.origin}/api/positions?_id=${positionId}`)
       .then((response) => {
         if (response.status !== 200) {
           return response.json()
@@ -50,4 +52,51 @@ window.onload = () => {
         errorMessage.innerText = error;
       });
   }
+
+  form.onsubmit = (event) => {
+    event.preventDefault();
+    saveButton.disabled = true;
+
+    let url;
+    const options = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        client: clientInput.value,
+        jobDescription: jobDescriptionInput.value,
+        vacancy: vacancyInput.value,
+        professionalProfiles: professionalProfilesInput.value,
+        isOpen: isOpenInput.value,
+      }),
+    };
+
+    if (positionId) {
+      options.method = 'PUT';
+      url = `${window.location.origin}/api/positions/${positionId}`;
+    } else {
+      options.method = 'POST';
+      url = `${window.location.origin}/api/positions`;
+    }
+
+    fetch(url, options)
+      .then((response) => {
+        if (response.status !== 200 && response.status !== 201) {
+          return response.json().then(({ message }) => {
+            throw new Error(message);
+          });
+        }
+        return response.json();
+      })
+      .then(() => {
+        // eslint-disable-next-line no-underscore-dangle
+        window.location.href = `${window.location.origin}/views/positionList.html`;
+      })
+      .catch((error) => {
+        errorMessage.innerText = error;
+      })
+      .finally(() => {
+        saveButton.disabled = false;
+      });
+  };
 };
