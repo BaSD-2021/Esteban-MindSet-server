0192c4344539b6de5a0a46617a884d979edc2320
joaquin leimeter <leimeter.joaquin@gmail.com>
2021-11-22 13:33:00 -0300

    MS-51: create html routes, add getInterviews function

--- public/js/interviewsList.js
+++ public/js/interviewsList.js
@@ -1,8 +1,7 @@
 const navButton = document.getElementById('interviewsNav');
-navButton.classList.add('activePage');
-
-const proceedButton = document.getElementById('proceed-button')
-
+const addInterviewButton = document.getElementById('addClient');
+const errorMessage = document.getElementById('error-message');
+const confirmDeleteButton = document.getElementById('procced-button');
 const tableContent = document.getElementById('table-content');
 
 const openEditInterviewsForm = (interview) => {
@@ -14,8 +13,6 @@ const openNewInterviewsForm = () => {
   // eslint-disable-next-line no-underscore-dangle
   window.location.href = `${window.location.origin}/views/interviewsForm.html`;
 };
-const addInterviewButton = document.getElementById('addClient');
-addInterviewButton.onclick = openNewInterviewsForm;
 
 let interviewId;
 
@@ -33,31 +30,63 @@ const deleteInterview = (interview, event) => {
   openModal();
 };
 
-fetch(`${window.location.origin}/api/interviews`)
-  .then((response) => response.json())
-  .then((response) => {
-    response.data.forEach((interview) => {
-      const tr = document.createElement('tr');
-      const dateTD = document.createElement('td');
-      const postulantNameTD = document.createElement('td');
-      const clientNameTD = document.createElement('td');
-      const actionsTD = document.createElement('td');
+const getInterviews = () => {
+  while (tableContent.firstChild) {
+    tableContent.removeChild(tableContent.firstChild);
+  }
 
-      const date = interview.date.substring(0, 10);
+  fetch(`${window.location.origin}/api/interviews`)
+    .then((response) => response.json())
+    .then((response) => {
+      response.data.forEach((interview) => {
+        const tr = document.createElement('tr');
+        const dateTD = document.createElement('td');
+        const postulantNameTD = document.createElement('td');
+        const clientNameTD = document.createElement('td');
+        const actionsTD = document.createElement('td');
 
-      dateTD.innerText = date;
-      postulantNameTD.innerText = `${interview?.postulant?.firstName} ${interview?.postulant?.lastName}`;
-      clientNameTD.innerText = interview?.client?.name;
+        const date = interview.date.substring(0, 10);
 
-      const button = document.createElement('button');
-      button.innerText = 'Delete';
+        dateTD.innerText = date;
+        postulantNameTD.innerText = `${interview?.postulant?.firstName} ${interview?.postulant?.lastName}`;
+        clientNameTD.innerText = interview?.client?.name;
 
-      // eslint-disable-next-line no-underscore-dangle
-      button.onclick = (event) => deleteInterview(interview, event);
-      actionsTD.append(button);
+        const button = document.createElement('button');
+        button.innerText = 'Delete';
 
-      tr.onclick = () => openEditInterviewsForm(interview);
-      tr.append(dateTD, postulantNameTD, clientNameTD, actionsTD);
-      tableContent.append(tr);
+        // eslint-disable-next-line no-underscore-dangle
+        button.onclick = (event) => deleteInterview(interview, event);
+        actionsTD.append(button);
+
+        tr.onclick = () => openEditInterviewsForm(interview);
+        tr.append(dateTD, postulantNameTD, clientNameTD, actionsTD);
+        tableContent.append(tr);
+      });
     });
-  });
+};
+
+confirmDeleteButton.onclick = () => {
+  const url = `${window.location.origin}/api/interviews/${interviewId}`;
+  fetch(url, {
+    method: 'DELETE',
+  })
+    .then((response) => {
+      if (response.status !== 204) {
+        return response.json().then(({ message }) => {
+          throw new Error(message);
+        });
+      }
+      getInterviews();
+      // eslint-disable-next-line no-undef
+      return closeModal();
+    })
+    .catch((error) => {
+      errorMessage.innerText = error;
+    });
+};
+
+addInterviewButton.onclick = openNewInterviewsForm;
+
+navButton.classList.add('activePage');
+
+getInterviews();
