34c199f27e756cb3dff93258beb7c3bed406e035
Nicolas Cagnina <cagnina.nicolas@gmail.com>
2021-11-22 14:17:08 -0300

    MS-55: code cleaning on sessions.js and validations file

--- validations/sessions.js
+++ validations/sessions.js
@@ -57,16 +57,16 @@ const validateSessions = (req, res, next) => {
 const validateSessionsUsedAttr = (req, res, next) => {
   const invalidBodyAttrs = [];
   if (req.body.postulant && !ObjectId.isValid(req.body.postulant)) {
-    invalidBodyAttrs.push("'Postulant ID'");
+    invalidBodyAttrs.push('Postulant ID');
   }
   if (req.body.psychologist && !ObjectId.isValid(req.body.psychologist)) {
-    invalidBodyAttrs.push("'Psychologist ID'");
+    invalidBodyAttrs.push('Psychologist ID');
   }
   if (req.body.status && !Object.values(statusEnum).includes(req.body.status)) {
-    invalidBodyAttrs.push("'Status'");
+    invalidBodyAttrs.push('Status');
   }
   if (req.body.date && !req.body.date.match(/^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2})$/)) {
-    invalidBodyAttrs.push("'Date'");
+    invalidBodyAttrs.push('Date');
   }
   if (invalidBodyAttrs.length === 1) {
     return errorResHelper(
