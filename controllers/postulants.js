56d02a0cf252b6b9081bb4052a26cae405ad0bad
Martin Raveglia <ravegliamartin@gmail.com>
2021-11-22 01:00:01 -0300

    MS-52: Add populate on listPostulants

--- controllers/postulants.js
+++ controllers/postulants.js
@@ -1,7 +1,7 @@
 const Postulants = require('../models/Postulants');
 
 const listPostulants = (req, res) => {
-  Postulants.find(req.query)
+  Postulants.find(req.query).populate('profiles.profileId', 'name')
     .then((postulants) => {
       res.status(200).json({
         message: 'List of Postulants',
