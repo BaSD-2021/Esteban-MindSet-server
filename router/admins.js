7ab31816965552d912267d872688bbf08ef19b34
Esteban Frare <esteban.frare@radiumrocket.com>
2021-11-25 12:16:15 -0300

    MS: admins routes

--- router/admins.js
+++ router/admins.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const controller = require('../controllers/admins');
+const validations = require('../validations/admins');
 
 const router = express.Router();
 
 const {
+  createAdmin,
   getAdmins,
+  deleteAdmin,
   updateAdmin,
 } = controller;
 
+router.post('/', validations.validateUpdatedAdmin, createAdmin);
+router.put('/:id', validations.validateUpdatedAdmin, updateAdmin);
+router.delete('/:id', validations.validateIdFormat, deleteAdmin);
 router.get('/', getAdmins);
-router.put('/:id', updateAdmin);
 
 module.exports = router;
