a51de8065e9769d8b24dcf9a3b4a53a871af52e3
Esteban Frare <esteban.frare@radiumrocket.com>
2021-12-21 17:16:08 -0300

    MS: implement Firebase

---
+++ src/helper/firebase/index.js
@@ -0,0 +1,11 @@
+const admin = require('firebase-admin');
+
+const firebaseApp = admin.initializeApp({
+  credential: admin.credential.cert({
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+  }),
+});
+
+module.exports = firebaseApp;
